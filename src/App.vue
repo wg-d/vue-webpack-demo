2b2b8af229953c1a043fc5b66fb8aab92f5e68f3
wg-d <wonderdream.dev@outlook.com>
2021-07-29 14:20:27 -0700

    bootstrap nav and table with example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline d-flex my-2 my-lg-0">
+        <input class="form-control mr-sm-2 mx-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+    </nav>
     <posts-example></posts-example>
   </div>
 </template>
@@ -18,12 +53,14 @@ export default {
 </script>
 
 <style>
+
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,5 +1,36 @@
 <template>
-    <h1>Posts works!</h1>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -7,11 +38,16 @@
 export default({
    name: 'PostsExample',
    data() {
-
+       return {
+           posts: [
+               { id: 1, title: 'Post1', body: 'This is the first cool post' },
+               { id: 2, title: 'Post2', body: 'This is the second cool post' },
+               { id: 3, title: 'Post3', body: 'This is the third cool post' },
+               { id: 4, title: 'Post4', body: 'This is the fourth cool post' },
+               { id: 5, title: 'Post5', body: 'This is the fifth cool post' },
+               { id: 6, title: 'Post6', body: 'This is the sixth cool post' },
+           ]
+       }
     }
 })
 </script>
-
-<style scoped>
-
-</style>
