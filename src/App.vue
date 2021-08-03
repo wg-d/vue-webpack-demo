d86a101717baa64499954afaec5ee651dd118254
wg-d <wonderdream.dev@outlook.com>
2021-08-03 08:35:34 -0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Blog Site</a>
-    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
-    <div class="navbar-collapse" id="navbarSupportedContent">
+    <nav class="navbar navbar-expand-lg text-white bg-primary d-flex justify-content-between px-4">
+    <a class="navbar-brand text-white" href="#">Blog Site</a>
+    <div class="" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link class="nav-link" :to='"/"'>Home</router-link>
+        <li class="nav-item">
+          <router-link class="nav-link text-white" :to='"/"'>Home</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" :to='"/contact"'>Blog</router-link>
+          <router-link class="nav-link text-white" :to='"/contact"'>Blog</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" :to='"/contact"'>Post</router-link>
+          <router-link class="nav-link text-white" :to='"/contact"'>Post</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" :to='"/contact"'>My Account</router-link>
+          <router-link class="nav-link text-white" :to='"/contact"'>My Account</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" :to='"/contact"'>Login</router-link>
+          <router-link class="nav-link text-white" :to='"/contact"'>Login</router-link>
         </li>
       </ul>
     </div>
